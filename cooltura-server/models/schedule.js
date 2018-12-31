25773b335cda7ce2c79aabded3db6462afc4c33f
Ismail <ismailkasep@gmail.com>
2018-12-31 21:52:57 +0700

    add config and models

---
+++ cooltura-server/models/schedule.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const Schedule = sequelize.define('schedule', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    hour: {
+      type: DataTypes.INTEGER,
+    },
+    minute: {
+      type: DataTypes.INTEGER,
+    },
+    delay_to_off: {
+      type: DataTypes.INTEGER,
+    },
+    delay_to_off_unit: {
+      type: DataTypes.STRING,
+    },
+    is_active: {
+      type: DataTypes.BOOLEAN,
+    },
+    is_deleted: {
+      type: DataTypes.BOOLEAN,
+    }
+  })
+
+  return Schedule;
+}
